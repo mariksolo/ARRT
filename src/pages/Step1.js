7f4882092ed0b0bbc7f534a1149873ebd8ae2b46
Anan <mengan.wang@gmail.com>
2020-01-26 11:29:09 -0800

    hi mark

--- src/pages/Step1.js
+++ src/pages/Step1.js
@@ -3,121 +3,163 @@ import React, { Component } from "react";
 import getCriticalInfo from "../api/getCriticalInfo";
 import getInteractions from "../api/getInteractions";
 import { findByLabelText } from "@testing-library/react";
-import { ListGroup, Col, Row, Accordion, Card, Button, Container } from "react-bootstrap";
+import {
+    ListGroup,
+    Col,
+    Row,
+    Accordion,
+    Card,
+    Button,
+    Container
+} from "react-bootstrap";
 
 export class step1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { info: {}, interactions: [] };
-  }
-  async componentDidMount() {
-    let information = await getCriticalInfo(this.props.drug);
-    // console.log(JSON.parse(info));
+    constructor(props) {
+        super(props);
+        this.state = { info: {}, interactions: [] };
+    }
+    async componentDidMount() {
+        let information = await getCriticalInfo(this.props.drug);
+        // console.log(JSON.parse(info));
 
-    // info = await info.json();
-    this.setState({ info: information }); //I have to make this so that it logs arrays within info
-    let interaction = await getInteractions(this.props.drug);
-    interaction = interaction.slice(0, 10);
-    // console.log(interaction);
-    this.setState({ interactions: interaction });
-    console.log(information);
-  }
+        // info = await info.json();
+        this.setState({ info: information }); //I have to make this so that it logs arrays within info
+        let interaction = await getInteractions(this.props.drug);
+        interaction = interaction.slice(0, 10);
+        // console.log(interaction);
+        this.setState({ interactions: interaction });
+        console.log(information);
+    }
 
-  // state = {
-  //     info: [],
-  //     interactions: []
-  // };
+    // state = {
+    //     info: [],
+    //     interactions: []
+    // };
 
-  render() {
-    return (
-      <Container className="pageContainer">
-      <div className="Step1">
-        <h1 className="StepHeader">Basic Drug Profile</h1>
+    render() {
+        return (
+            <Container className="pageContainer">
+                <div className="Step1">
+                    <Row>
+                        <Col md={12}>
+                            <h2>{this.state.info.generic_name}</h2>
+                        </Col>
 
-        <Row>
-          <Col md={3}>
-            <Accordion defaultActiveKey="0">
-              <Card>
-                {/* <Card.Header> */}
-                  <Accordion.Toggle as={Button} variant="primary" eventKey="0">
-                    Active Ingredient
-                  </Accordion.Toggle>
-                {/* </Card.Header> */}
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <p>{this.state.info.active_ingredient}</p>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                {/* <Card.Header> */}
-                  <Accordion.Toggle as={Button} variant="secondary" eventKey="1">
-                    Generic Name
-                  </Accordion.Toggle>
-                {/* </Card.Header> */}
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>
-                    <p>{this.state.info.generic_name}</p>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                {/* <Card.Header> */}
-                  <Accordion.Toggle as={Button} variant="info" eventKey="2">
-                    Route
-                  </Accordion.Toggle>
-                {/* </Card.Header> */}
-                <Accordion.Collapse eventKey="2">
-                  <Card.Body>
-                    <p>{this.state.info.route}</p>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-              <Card>
-                {/* <Card.Header> */}
-                  <Accordion.Toggle as={Button} variant="dark" eventKey="3">
-                    Substance Name
-                  </Accordion.Toggle>
-                {/* </Card.Header> */}
-                <Accordion.Collapse eventKey="3">
-                  <Card.Body>
-                    <p>{this.state.info.substance_name}</p>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          </Col>
+                        <Col md={12}>
+                            <p>Generic Name: {this.state.info.generic_name}</p>
+                            <p>
+                                Substance Name: {this.state.info.substance_name}
+                            </p>
+                        </Col>
 
-          <Col md={5}>
-            {/* <Row> */}
-              <h5>Purpose</h5>
-              <p>{this.state.info.purpose}</p>
-            {/* </Row>
+                        <Col md={3}>
+                            <Accordion defaultActiveKey="0">
+                                <Card>
+                                    {/* <Card.Header> */}
+                                    <Accordion.Toggle
+                                        as={Button}
+                                        variant="primary"
+                                        eventKey="0"
+                                    >
+                                        Active Ingredient
+                                    </Accordion.Toggle>
+                                    {/* </Card.Header> */}
+                                    <Accordion.Collapse eventKey="0">
+                                        <Card.Body>
+                                            <p>
+                                                {
+                                                    this.state.info
+                                                        .active_ingredient
+                                                }
+                                            </p>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                <Card>
+                                    {/* <Card.Header> */}
+                                    <Accordion.Toggle
+                                        as={Button}
+                                        variant="secondary"
+                                        eventKey="1"
+                                    >
+                                        Generic Name
+                                    </Accordion.Toggle>
+                                    {/* </Card.Header> */}
+                                    <Accordion.Collapse eventKey="1">
+                                        <Card.Body>
+                                            <p>
+                                                {this.state.info.generic_name}
+                                            </p>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                <Card>
+                                    {/* <Card.Header> */}
+                                    <Accordion.Toggle
+                                        as={Button}
+                                        variant="info"
+                                        eventKey="2"
+                                    >
+                                        Route
+                                    </Accordion.Toggle>
+                                    {/* </Card.Header> */}
+                                    <Accordion.Collapse eventKey="2">
+                                        <Card.Body>
+                                            <p>{this.state.info.route}</p>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                <Card>
+                                    {/* <Card.Header> */}
+                                    <Accordion.Toggle
+                                        as={Button}
+                                        variant="dark"
+                                        eventKey="3"
+                                    >
+                                        Substance Name
+                                    </Accordion.Toggle>
+                                    {/* </Card.Header> */}
+                                    <Accordion.Collapse eventKey="3">
+                                        <Card.Body>
+                                            <p>
+                                                {this.state.info.substance_name}
+                                            </p>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            </Accordion>
+                        </Col>
+
+                        <Col md={5}>
+                            {/* <Row> */}
+                            <h5>Purpose</h5>
+                            <p>{this.state.info.purpose}</p>
+                            {/* </Row>
             <Row className="Usage"> */}
-              <h5>Indications and Usage</h5>
-              <p>{this.state.info.indications_and_usage}</p>
-              <h5>Product Type</h5>
-              <p>{this.state.info.product_type}</p>
-            {/* </Row> */}
-          </Col>
-          <Col md={4} className="Interactions">
-            <h5>Substance Interactions</h5>
-            <ListGroup>
-              {this.state.interactions.map(drug => (
-                <ListGroup.Item>{drug}</ListGroup.Item>
-              ))}
-              {/* <ListGroup.Item>Hi</ListGroup.Item> */}
-            </ListGroup>
-          </Col>
-        </Row>
-        <Row md={2}>
-            <h5>Warnings</h5>
-            <p>{this.state.info.warnings}</p>
-        </Row>
-      </div>
-      </Container>
-    );
-  }
+                            <h5>Indications and Usage</h5>
+                            <p>{this.state.info.indications_and_usage}</p>
+                            <h5>Product Type</h5>
+                            <p>{this.state.info.product_type}</p>
+                            {/* </Row> */}
+                        </Col>
+                        <Col md={4} className="Interactions">
+                            <h5>Substance Interactions</h5>
+                            <ListGroup>
+                                {this.state.interactions.map(drug => (
+                                    <ListGroup.Item>{drug}</ListGroup.Item>
+                                ))}
+                                {/* <ListGroup.Item>Hi</ListGroup.Item> */}
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                    <Row md={2}>
+                        <h5>Warnings</h5>
+                        <p>{this.state.info.warnings}</p>
+                    </Row>
+                </div>
+            </Container>
+        );
+    }
 }
 
 export default step1;
--- src/pages/Step2.js
+++ src/pages/Step2.js
@@ -4,59 +4,80 @@ import getCriticalInfo from "../api/getCriticalInfo";
 import getInteractions from "../api/getInteractions";
 import { findByLabelText } from "@testing-library/react";
 import {
-  ListGroup,
-  Col,
-  Row,
-  Accordion,
-  Card,
-  Button,
-  Container
+    ListGroup,
+    Col,
+    Row,
+    Accordion,
+    Card,
+    Button,
+    Container
 } from "react-bootstrap";
 
 export class step2 extends Component {
-  async componentDidMount() {
-    let information = await getCriticalInfo(this.props.drug);
-    // console.log(JSON.parse(info));
+    async componentDidMount() {
+        let information = await getCriticalInfo(this.props.drug);
+        // console.log(JSON.parse(info));
 
-    // info = await info.json();
-    this.setState({ info: information }); //I have to make this so that it logs arrays within info
-    let interaction = await getInteractions(this.props.drug);
-    console.log(interaction);
-    this.setState({ interactions: interaction });
-  }
+        // info = await info.json();
+        this.setState({ info: information }); //I have to make this so that it logs arrays within info
+        let interaction = await getInteractions(this.props.drug);
+        console.log(interaction);
+        this.setState({ interactions: interaction });
+    }
 
-  state = {
-    info: [],
-    interactions: []
-  };
+    state = {
+        info: [],
+        interactions: []
+    };
 
-  render() {
-    return (
-      <Container className="pageContainer">
-        <div className="Step2">
-          <h1 className="StepHeader headerColors">Warnings</h1>
+    render() {
+        return (
+            <Container className="pageContainer">
+                <div className="Step2">
+                    <h1 className="StepHeader headerColors">Warnings</h1>
 
-          <Row>
-            <Col md={6}>
-              <h5 className="headerColors">Do Not Use</h5>
-              <p>{this.state.info.do_not_use}</p>
-            </Col>
+                    <Row>
+                        <Col md={6}>
+                            <h5 className="headerColors">Do Not Use</h5>
+                            <p>{this.state.info.do_not_use}</p>
+                        </Col>
 
-            <Col md={6}>
-              <h5 className="headerColors">Ask Doctor</h5>
-              <p>{this.state.info.ask_doctor}</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12}>
-              <h5 className="headerColors">Ask Pharmacist</h5>
-              <p>{this.state.info.ask_doctor_or_pharmacist}</p>
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    );
-  }
+                        <Col md={6}>
+                            <h5 className="headerColors">Ask Doctor</h5>
+                            <p>{this.state.info.ask_doctor}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
+                            <h5 className="headerColors">Ask Pharmacist</h5>
+                            <p>{this.state.info.ask_doctor_or_pharmacist}</p>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col md={12}>
+                            <div className="Interactions">
+                                <h5 className="headerColors">
+                                    Storage and Handling
+                                </h5>
+
+                                <p>{this.state.storage_and_handling}</p>
+                            </div>
+
+                            <div className="Purpose">
+                                <h5 className="headerColors">
+                                    Dosage and Administration
+                                </h5>
+                                <p>
+                                    {this.state.info.dosage_and_administration}
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+        );
+    }
 }
 
 export default step2;

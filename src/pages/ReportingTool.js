4a4ccad9eb78c9b41ce62bbfbc993e1ee7e72cae
Anan <mengan.wang@gmail.com>
2020-02-29 17:26:58 -0800

    idk what this is

--- src/pages/ReportingTool.js
+++ src/pages/ReportingTool.js
@@ -14,12 +14,33 @@ import {
     Form
 } from "react-bootstrap";
 
+import firebase from "firebase";
+
 export class ReportingTool extends Component {
     constructor(props) {
         super(props);
     }
+
+    componentDidMount() {}
     handleSubmit = event => {
         event.preventDefault();
+
+        var firebaseConfig = {
+            apiKey: "AIzaSyBcn-5v35vP2kSRDdfec7MEanQPMfO48fQ",
+            authDomain: "arrtdatabase.firebaseapp.com",
+            databaseURL: "https://arrtdatabase.firebaseio.com",
+            projectId: "arrtdatabase",
+            storageBucket: "arrtdatabase.appspot.com",
+            messagingSenderId: "479769423414",
+            appId: "1:479769423414:web:e4f87b5350859e56103297"
+        };
+
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        // Initialize Firebase
+        // firebase.initializeApp(firebaseConfig);
+        // firebase.analytics();
+        var db = firebase.firestore();
 
         this.setState({
             interactions: [...this.state.interactions, this.state.drug1]
@@ -30,6 +51,20 @@ export class ReportingTool extends Component {
         });
 
         console.log(this.state);
+
+        db.collection("reports")
+            .add({
+                drug1: this.state.drug1,
+                drug2: this.state.drug2,
+                interactions: this.state.interactions
+                // penis: "penis"
+            })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
     };
 
     state = {
